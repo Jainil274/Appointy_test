97ff807857478eb19535ea7b2558dc8fd5f18c9c
Jainil274 <61516033+Jainil274@users.noreply.github.com>
2021-10-09 20:46:56 +0530

    Delete client/core directory

--- client/core/constant.js
+++
@@ -1,6 +0,0 @@
-define([], function() {
-    'use strict';
-    var constant = angular.module('constant', [])
-        .constant('INSTA_API_URL', 'http://localhost:8999');
-    return constant;
-});
